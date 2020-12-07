7faa7c5134148b2d83ebe913f4f6c62f49469d10
Ada64 <55141583+Ada64@users.noreply.github.com>
2020-12-08 00:39:33 +0200

    removed christmas themed status

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot = commands.Bot(command_prefix=".")
 @bot.event
 async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=discord.Activity(
-        type=discord.ActivityType.playing, name='Merry X-Mas! | .help'
+        type=discord.ActivityType.watching, name=f'{len(bot.guilds)} servers | .help'
     ))
     print("Notification | Bot has been turned on!")
 
@@ -287,7 +287,7 @@ async def unban(ctx, *, member):
       await ctx.guild.unban(user)
       await ctx.send(f'Unbanned {user.mention}.')
       return
-@bot.command(aliases=['dicerolle'])
+@bot.command(aliases=['dicerolll'])
 async def diceroll(ctx):
     responses = ['You rolled a 1!',
     'You rolled a 2!',
